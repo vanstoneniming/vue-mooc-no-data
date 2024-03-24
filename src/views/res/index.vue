e6809d4edc4b47285ceaf1a321f1bb07ef402f8b
niming <michaelmingzi@gmail.com>
2024-03-24 17:29:03 +0800

    更新

--- src/views/res/index.vue
+++ src/views/res/index.vue
@@ -1,48 +1,60 @@
 <template>
   <div class="home">
+    <header-short-cut/>
     <ul>
-      <li v-for="(item, index) in dataList"
-          :key="index" @click="goToDetail(item)"
-      >
-        {{ item.id }}
-        {{ item.course_name }}
-        {{ item.title }}
+      <li v-for="(item, index) in dataList" v-bind:key="index">
+        <div class="ceci-item">
+          <resource-list :data="item"/>
+        </div>
       </li>
     </ul>
+    <div class="pagination">
+      <el-pagination
+        v-model:current-page="currentPage"
+        v-model:page-size="pageSize"
+        :hide-on-single-page=true
+        :page-sizes="[12, 24, 36, 48, 60]"
+        :total="totalSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </div>
   </div>
-    <el-pagination
-    v-model:current-page="currentPage"
-    v-model:page-size="pageSize"
-    :page-sizes="[10, 20, 50, 100]"
-    layout="total, sizes, prev, pager, next, jumper"
-    :total="totalSize"
-    @size-change="handleSizeChange"
-    @current-change="handleCurrentChange"
-  />
 </template>
-
 <script lang="ts">
-import { defineComponent, onBeforeMount, ref } from 'vue'
+import { defineComponent, onBeforeMount, ref, watch } from 'vue'
 import { getRes } from '@/api/common'
 import { ResConfig } from '@/types'
 import { ERR_SUCCESS } from '@/api/config'
 import router from '@/router'
+import ResourceDetail from '@/components/resource/index.vue'
+import bus from '@/utils/bus'
+import { ElPagination } from 'element-plus/lib'
+import { useRoute } from 'vue-router'
+import HeaderShortCut from '@/components/header/modules/ShortCut.vue'
 
 export default defineComponent({
-  name: 'Footer',
+  name: 'Resource',
+  components: { HeaderShortCut, ElPagination, ResourceList: ResourceDetail },
   setup () {
     const dataList = ref<ResConfig[]>([])
     const currentPage = ref<number>(1)
-    const pageSize = ref<number>(10)
-    const totalSize = ref<number>(50)
+    const pageSize = ref<number>(12)
+    const totalSize = ref<number>(0)
+    const searchKeyword = ref('')
+    const route = useRoute()
 
     async function fetchData () {
       try {
         const { code, result: { items: data, total } } = await getRes({
           params:
-              {
-                page: currentPage.value, pageSize: pageSize.value
-              }
+            {
+              page: currentPage.value,
+              pageSize: pageSize.value,
+              title: searchKeyword.value,
+              course: router.currentRoute.value.query.course
+            }
         })
         if (code === ERR_SUCCESS && data) {
           dataList.value = data
@@ -52,9 +64,16 @@ export default defineComponent({
         console.error('Error fetching data:', error)
       }
     }
+
+    bus.on('keywordChange', (event) => {
+      searchKeyword.value = event as string
+      fetchData()
+    })
+
     const handleSizeChange = () => {
       fetchData()
     }
+
     function handleCurrentChange () {
       fetchData()
     }
@@ -63,29 +82,37 @@ export default defineComponent({
       fetchData()
     }
     )
-    const goToDetail = (item: ResConfig) => {
-      router.push({ name: 'ResourceDetail', params: { id: item.id } })
-    }
+
+    watch(
+      () => route,
+      () => {
+        fetchData()
+      },
+      { deep: true }
+    )
 
     return {
       dataList,
       currentPage,
       pageSize,
       totalSize,
-      goToDetail,
       handleCurrentChange,
       handleSizeChange
     }
   }
+
 })
 </script>
 
 <style scoped>
-  @import '~@/assets/styles/responsive.scss';
-  .home {
-      margin: 10px 20px;
-  }
-  li {
-    padding: 10px;
-  }
+@import '~@/assets/styles/responsive.scss';
+
+.home {
+  margin: 10px 20px;
+}
+
+li {
+  padding: 10px;
+  cursor: grab;
+}
 </style>
