37d59c2acb9c9d22233f4d7fdbc63d62ad0310a7
niming <michaelmingzi@gmail.com>
2024-07-11 20:04:39 +0800

    更新

--- src/components/footer/ShortCut.vue
+++ src/components/footer/ShortCut.vue
@@ -6,7 +6,7 @@
       </el-icon>
       返回
     </el-button>
-    <el-button @click="goShare">
+    <el-button>
       <el-icon size="24px">
         <Share/>
       </el-icon>
@@ -14,29 +14,14 @@
     </el-button>
   </div>
 </template>
-<script lang="ts">
-import router from '@/router'
-import { ElMessage } from 'element-plus/lib'
 
-export default {
-  name: 'HeaderShortCut',
-  methods: {
-    goBack () {
-      router.go(-1)
-    },
-    goShare () {
-      const currentURL = window.location.href
-      const text = `资源链接： ${currentURL}`
-      navigator.clipboard.writeText(text).then(() => {
-        ElMessage.success('链接已复制到剪贴板！')
-      })
-        .catch((error) => {
-          ElMessage.error('链接复制失败: ', error)
-        })
-    }
-  }
+<script lang="ts" setup name="HeaderShortCut">
+import router from '@/router'
+function goBack () {
+  router.go(-1)
 }
 </script>
+
 <style scoped>
 @import '~@/assets/styles/responsive.scss';
 
